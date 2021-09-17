8057bba8b383185e2a447a9f60314fc8edba0392
Sotaro Tommykawa <tommysouta@gmail.com>
2021-09-17 14:53:17 +0900

    add carbon-svelte

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,12 @@
 import adapt from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
+import { optimizeImports } from 'carbon-preprocess-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [preprocess(), optimizeImports()],
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
