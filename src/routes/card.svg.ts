18bf41832bd3f767f7e1323127e592f0c4fab42b
Sotaro Tommykawa <tommysouta@gmail.com>
2021-09-17 20:00:12 +0900

    fix type for linter

--- src/routes/card.svg.ts
+++ src/routes/card.svg.ts
@@ -2,14 +2,14 @@ import type { RequestHandler } from '@sveltejs/kit';
 import Svg from '$lib/svg/Svg.svelte';
 
 type SvelteComponent = {
-	render: (props: any) => { html: string; css: { code: string } };
+	render: (props: unknown) => { html: string; css: { code: string } };
 };
 
 export const get: RequestHandler = ({ query }) => {
 	const name = query.get('name');
 	const twitter = query.get('twitter');
 	const github = query.get('github');
-	const { html, css } = ((Svg as unknown) as SvelteComponent).render({ name, twitter, github });
+	const { html, css } = (Svg as unknown as SvelteComponent).render({ name, twitter, github });
 	return {
 		headers: {
 			'Content-type': 'image/svg+xml'
