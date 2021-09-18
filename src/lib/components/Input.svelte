dd74f7546a2d27cbc0f2c8a1ce3b765a336458e2
ssssota <tommysouta@gmail.com>
2021-09-18 18:52:18 +0900

    add styled input

---
+++ src/lib/components/Input.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	import type { InputEvent } from './types';
+	export let type: string = 'text';
+	export let value: string | number | null | undefined = undefined;
+	export let placeholder: string = '';
+
+	type CustomEvent = {
+		input: InputEvent;
+	};
+
+	let computedType = type;
+	$: computedType = type || computedType;
+
+	let computedValue: string | number = '';
+	$: computedValue = value ?? '';
+
+	const dispatcher = createEventDispatcher<CustomEvent>();
+
+	const handleInput = (ev: InputEvent) => {
+		value = ev.currentTarget.value;
+		dispatcher('input', ev);
+	};
+</script>
+
+<span class="p-2 rounded shadow focus-within:ring inline-flex {$$props.class}">
+	<slot name="prefix" />
+	<input
+		class="focus:outline-none flex-grow"
+		type={computedType}
+		value={computedValue}
+		{placeholder}
+		on:input={handleInput}
+		on:blur
+		on:change
+		on:focus
+		on:keydown
+		on:keypress
+		on:keyup
+	/>
+	<slot name="suffix" />
+</span>
