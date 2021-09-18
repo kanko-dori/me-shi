83a44aa4e57e18eeacd8c867aadb2436299cc0c2
ssssota <tommysouta@gmail.com>
2021-09-18 18:52:58 +0900

    add demo page

---
+++ src/routes/components_demo.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import { Input, SuggestableInput, SuggestableTagInput } from '$lib/components';
+	let value = '';
+	let tags: string[] = [];
+</script>
+
+<div class="m-2 p-4 shadow">
+	<p>Input</p>
+	<Input bind:value placeholder="test" class="m-2 w-full">
+		<span slot="prefix"> This is prefix</span>
+		<span slot="suffix">This is suffix</span>
+	</Input>
+	<p>{value}</p>
+</div>
+
+<div class="m-2 p-4 shadow">
+	<p>SuggestableInput</p>
+	<SuggestableInput
+		bind:value
+		placeholder="test"
+		class="m-2 w-full"
+		candidates={[
+			'ハックツハッカソン プレシオ杯',
+			'ハックツハッカソン スピノカップ',
+			'サマーハッカソン'
+		]}
+	>
+		<span slot="prefix"> This is prefix</span>
+		<span slot="suffix">This is suffix</span>
+	</SuggestableInput>
+	<p>{value}</p>
+</div>
+
+<div class="m-2 p-4 shadow">
+	<p>SuggestableTagInput</p>
+	<SuggestableTagInput
+		bind:value={tags}
+		placeholder="test"
+		class="m-2 w-full"
+		candidates={['Go', 'TypeScript', 'Kubernetes', 'CSS', 'Docker', 'Rust', 'WebAssembly', 'C']}
+	>
+		<span slot="prefix"> This is prefix</span>
+		<span slot="suffix">This is suffix</span>
+	</SuggestableTagInput>
+	<p>{tags.join(', ')}</p>
+</div>
