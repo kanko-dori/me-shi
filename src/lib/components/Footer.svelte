e7039d2887de3f55bdb9cd412c10b8e263f3ba96
Sotaro Tommykawa <tommysouta@gmail.com>
2021-09-18 23:54:21 +0900

    add header and footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { assets } from '$app/paths';
+</script>
+
+<footer class="py-8 pt-16">
+	<p class="text-center"><small> &copy; 閑古鳥 </small></p>
+	<p class="text-center pt-4"><img class="inline" src="{assets}/kanko-dori.svg" alt="閑古鳥" /></p>
+</footer>
---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import Logo from '$lib/svg/Logo.svelte';
+	import Svg from '$lib/svg/Svg.svelte';
+
+	export let githubId: string | undefined = undefined;
+</script>
+
+<header class="py-4" role="banner" aria-label="me-shi">
+	<div class="container mx-auto flex items-center">
+		<a href="/" class="outline-none focus:ring-2">
+			<Svg width={200} height={50}>
+				<Logo scale={5} x={2} y={1} />
+			</Svg>
+		</a>
+		<div class="flex-grow" />
+		<p class="pr-4 text-right">
+			{#if githubId === undefined}
+				Sign in with GitHub
+			{:else}
+				<img
+					class="h-10 rounded-full ring-2 focus:ring-4"
+					src="https://github.com/{githubId}.png"
+					alt="user icon"
+				/>
+			{/if}
+		</p>
+	</div>
+</header>
