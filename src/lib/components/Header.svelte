c3f4fe5228ea4f45468454fd3298cae08b366632
kambe <nori.k3636@gmail.com>
2021-09-19 00:14:08 +0900

    試行錯誤中

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,16 @@
+<script>
+</script>
+
+<header>
+	<div class="flex bg-blue-300">
+		<div class="flex-1 m-8">
+			<img class=" w-3/4" src="../static/logo.svg" alt="logo" />
+		</div>
+		<div class="flex-1 my-auto text-center">
+			<a href="/#" class="text-base md:4xl no-underline">sign in with github</a>
+		</div>
+	</div>
+</header>
+
+<style>
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,37 @@
 <script>
+	import { Static } from '$lib/svg';
+	import Header from '$lib/components/Header.svelte';
 </script>
 
 <div />
+<Header />
+<div class="bg-blue-300">
+	<div class="card1" />
+	<div class="card2" />
+</div>
+<div class="bg-blue-200 h-64">
+	<div class="absolute opacity-75 border-4 h-1/3 w-1/2 z-0 mt-0 bg-blue-100 border-gray-800" />
+	<div class="absolute opacity-75 border-4 h-1/3 w-1/2 z-10 mt-1 bg-blue-400 border-gray-800" />
+	<div class="absolute bg-blue-300 z-20 mt-2">
+		<p>名刺でつながる、</p>
+		<p>名刺でつなげる。</p>
+	</div>
+</div>
 
+<!-- <div>
+	<Static name="閑古鳥" github="kanko-dori" />
+</div> -->
 <style>
+	div.card1 {
+		position: absolute;
+		background-color: antiquewhite;
+		height: 55px;
+		width: 99px;
+	}
+	div.card2 {
+		position: absolute;
+		background-color: antiquewhite;
+		height: 10px;
+		width: 10px;
+	}
 </style>
