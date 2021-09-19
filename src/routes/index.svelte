9686841c6226e44cbe71c7e06eab227ae8b2b596
kambe <nori.k3636@gmail.com>
2021-09-20 00:38:46 +0900

    except for p

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,58 +7,52 @@
 <Header />
 <div class="container mx-auto">
 	<div class="relative aspect-w-2 aspect-h-1">
-		<div class="card">
-			<div class="top">top</div>
-			<div class="botom">botom</div>
-			<div class="copy">
+		<div class="relative">
+			<div class="top absolute z-10 w-1/2 opacity-75 border-2 border-gray-600 bg-gray-100">top</div>
+			<div class="bottom absolute z-0 top-0 w-1/2 opacity-75 border-2 border-gray-600 bg-gray-100">
+				bottom
+			</div>
+			<div class="catch-copy absolute z-20">
 				<p>名刺でつながる、</p>
 				<p>名刺がつなげる。</p>
 			</div>
 		</div>
-		<!-- <dev class="static">
-		<p>イベントで見つけたあの人ともう少しだけつながってみませんか？</p>
-		<p>me-shiは、あと「少しだけ」つながる場を提供します。</p>
-		<div class="static aspect-w-10 aspect-h-5">
-			<Static name="閑古鳥" github="kanko-dori" />
-		</div>
-	</dev>
-
-	<p>つながった人には感想を残しましょう</p>
-	<p>成果物の感想欲しくない？？</p> -->
 	</div>
+</div>
+<div class="containar mx-auto relative">
+	<div class="aspect-w-6 aspect-h-1 bg-gray-300">
+		<div class="absolute p-left">
+			<p>イベントで見つけたあの人ともう少しだけつながってみませんか？</p>
+			<p>me-shiは、あと「少しだけ」つながる場を提供します。</p>
+		</div>
+	</div>
+</div>
+<div class="aspect-w-2 aspect-h-1 w-3/4 mx-auto">
+	<Static name="閑古鳥" github="kanko-dori" />
+</div>
+<div class="absolute p-left">
+	<p>つながった人には感想を残しましょう</p>
+	<p>成果物の感想欲しくない？？</p>
 </div>
 
 <Footer />
 
 <style>
-	.card {
-		position: relative;
-	}
-	.top,
-	.botom {
-		position: absolute;
-		border-color: gray;
-		background-color: ghostwhite;
-		border-width: 0.1em;
-		width: 50%;
-		padding-top: calc(50% * 0.6);
-		opacity: 0.7;
-	}
-	.botom {
-		z-index: 1;
-		top: 0;
-		right: 15%;
-	}
 	.top {
-		z-index: 2;
-		margin-top: 10%;
-		left: 15%;
+		padding-top: 20vw;
+		margin-top: 10vw;
+		left: 5vw;
 	}
-	.copy {
-		position: absolute;
-		left: 25%;
-		z-index: 3;
-		margin-top: 10%;
+	.bottom {
+		padding-top: 20vw;
+		right: 10vw;
+	}
+	.catch-copy {
+		left: 10vw;
+		margin-top: 10vw;
 		font-size: 6vw;
+	}
+	.p-left {
+		left: 10vw;
 	}
 </style>
