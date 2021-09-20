6e81a6a19dea6b0208e8926072d540e9cd7a702d
kambe <nori.k3636@gmail.com>
2021-09-20 23:01:04 +0900

    add fontsize

--- src/routes/kaerareru.svelte
+++ src/routes/kaerareru.svelte
@@ -13,23 +13,29 @@
 </div>
 <div class="container mx-auto">
 	<div class="m-4 p-8 border">
-		<p>Name</p>
+		<p class="t-size">Name</p>
 		<Input bind:value={myname} placeholder="" class="m-2 w-full" />
 	</div>
 	<div class="m-4 p-8 border">
-		<p>GitHub ID</p>
+		<p class="t-size">GitHub ID</p>
 		<Input bind:value={githubId} placeholder="" class="m-2 w-full" />
 	</div>
 	<div class="m-4 p-8 border">
-		<p>twitter ID</p>
+		<p class="t-size">twitter ID</p>
 		<Input bind:value={twitterId} placeholder="" class="m-2 w-full" />
 	</div>
 </div>
 
 <div class="container mx-auto flex px-8">
-	<Button class="bg-gray-300 ">Cancel</Button>
+	<Button class="bg-gray-300">Cancel</Button>
 	<div class="flex-grow" />
-	<Button class="bg-blue-600 text-white">Apply</Button>
+	<Button class="bg-blue-600 text-white t-size">Apply</Button>
 </div>
 
 <Footer />
+
+<style>
+	.t-size {
+		font-size: 3vw;
+	}
+</style>
