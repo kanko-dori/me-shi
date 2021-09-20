cecbc3776a18ad74f41a740a45b6acacebb68876
kambe <nori.k3636@gmail.com>
2021-09-20 23:23:27 +0900

    add tentative function

--- src/routes/kaerareru.svelte
+++ src/routes/kaerareru.svelte
@@ -1,35 +1,41 @@
 <script lang="ts">
 	import { Header, Input, Button, Footer } from '$lib/components';
 	import Static from '$lib/svg/Static.svelte';
-	let myname: string = '';
-	let githubId: undefined | string = undefined;
-	let twitterId: undefined | string = undefined;
+	let name: string = '';
+	let github: undefined | string = undefined;
+	let twitter: undefined | string = undefined;
+	function cancelClick() {
+		alert('Cancel Clicked');
+	}
+	function applyClick() {
+		alert('apply Clicked');
+	}
 </script>
 
 <Header />
 
 <div class="aspect-w-3 aspect-h-1 mx-auto">
-	<Static name={myname} github={githubId} twitter={twitterId} />
+	<Static {name} {github} {twitter} />
 </div>
 <div class="container mx-auto">
 	<div class="m-4 p-8 border">
 		<p class="t-size">Name</p>
-		<Input bind:value={myname} placeholder="" class="m-2 w-full" />
+		<Input bind:value={name} placeholder="" class="m-2 w-full" />
 	</div>
 	<div class="m-4 p-8 border">
 		<p class="t-size">GitHub ID</p>
-		<Input bind:value={githubId} placeholder="" class="m-2 w-full" />
+		<Input bind:value={github} placeholder="" class="m-2 w-full" />
 	</div>
 	<div class="m-4 p-8 border">
 		<p class="t-size">twitter ID</p>
-		<Input bind:value={twitterId} placeholder="" class="m-2 w-full" />
+		<Input bind:value={twitter} placeholder="" class="m-2 w-full" />
 	</div>
 </div>
 
 <div class="container mx-auto flex px-8">
-	<Button class="bg-gray-300">Cancel</Button>
+	<Button class="bg-gray-300" on:click={cancelClick}>Cancel</Button>
 	<div class="flex-grow" />
-	<Button class="bg-blue-600 text-white t-size">Apply</Button>
+	<Button class="bg-blue-600 text-white" on:click={applyClick}>Apply</Button>
 </div>
 
 <Footer />
