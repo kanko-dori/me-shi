55defd1dbcae1f41de4b26fd03365fb82bd6ecdc
kambe <nori.k3636@gmail.com>
2021-09-20 22:02:01 +0900

    add button

--- src/routes/kaerareru.svelte
+++ src/routes/kaerareru.svelte
@@ -1,31 +1,36 @@
-<script>
+<script lang="ts">
 	import { Header, Input, Footer } from '$lib/components';
 	import Static from '$lib/svg/Static.svelte';
-	let myname = '';
-	let githubId = '';
-	let twitterId = '';
+	let myname: string = '';
+	let githubId: undefined | string = undefined;
+	let twitterId: undefined | string = undefined;
+	function cancel() {}
+	function apply() {}
 </script>
 
 <Header />
 
 <div class="aspect-w-3 aspect-h-1 mx-auto">
-	<Static name={myname} github={githubId} />
+	<Static name={myname} github={githubId} twitter={twitterId} />
 </div>
 
 <div class="m-4 p-8 border">
 	<p>Name</p>
 	<Input bind:value={myname} placeholder="" class="m-2 w-full" />
-	<p>{myname}</p>
 </div>
 <div class="m-4 p-8 border">
 	<p>GitHub ID</p>
 	<Input bind:value={githubId} placeholder="" class="m-2 w-full" />
-	<p>{githubId}</p>
 </div>
 <div class="m-4 p-8 border">
 	<p>twitterId</p>
 	<Input bind:value={twitterId} placeholder="" class="m-2 w-full" />
-	<p>{twitterId}</p>
+</div>
+
+<div class="container mx-auto flex">
+	<button class="border" on:click={cancel}>Cancel</button>
+	<div class="flex-grow" />
+	<button class="border" on:click={apply}>Apply</button>
 </div>
 
 <Footer />
