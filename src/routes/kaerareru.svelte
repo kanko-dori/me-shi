ff9bc8c86ec65f508423b059896134d392b6f014
kambe <nori.k3636@gmail.com>
2021-09-20 22:26:28 +0900

    change responsive

--- src/routes/kaerareru.svelte
+++ src/routes/kaerareru.svelte
@@ -11,22 +11,23 @@
 <div class="aspect-w-3 aspect-h-1 mx-auto">
 	<Static name={myname} github={githubId} twitter={twitterId} />
 </div>
+<div class="container mx-auto">
+	<div class="m-4 p-8 border">
+		<p>Name</p>
+		<Input bind:value={myname} placeholder="" class="m-2 w-full" />
+	</div>
+	<div class="m-4 p-8 border">
+		<p>GitHub ID</p>
+		<Input bind:value={githubId} placeholder="" class="m-2 w-full" />
+	</div>
+	<div class="m-4 p-8 border">
+		<p>twitter ID</p>
+		<Input bind:value={twitterId} placeholder="" class="m-2 w-full" />
+	</div>
+</div>
 
-<div class="m-4 p-8 border">
-	<p>Name</p>
-	<Input bind:value={myname} placeholder="" class="m-2 w-full" />
-</div>
-<div class="m-4 p-8 border">
-	<p>GitHub ID</p>
-	<Input bind:value={githubId} placeholder="" class="m-2 w-full" />
-</div>
-<div class="m-4 p-8 border">
-	<p>twitterId</p>
-	<Input bind:value={twitterId} placeholder="" class="m-2 w-full" />
-</div>
-
-<div class="flex px-8">
-	<Button class="bg-gray-300">Cancel</Button>
+<div class="container mx-auto flex px-8">
+	<Button class="bg-gray-300 ">Cancel</Button>
 	<div class="flex-grow" />
 	<Button class="bg-blue-600 text-white">Apply</Button>
 </div>
