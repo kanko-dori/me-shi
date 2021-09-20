95171f5717adc4f28dab8845808b444f9fabbb41
kambe <nori.k3636@gmail.com>
2021-09-21 03:21:24 +0900

    fix mirareru

--- src/routes/mirareru.svelte
+++ src/routes/mirareru.svelte
@@ -1,8 +1,7 @@
 <script>
 	import { Header, Footer, Input, Button } from '$lib/components';
 	import { Dynamic, Static } from '$lib/svg';
-	import { QrCode16 } from 'carbon-icons-svelte';
-	import CaretRightGlyph from 'carbon-icons-svelte/lib/CaretRightGlyph';
+	import { QrCode16, SendFilled32 } from 'carbon-icons-svelte';
 
 	// 借値
 	let name = 'name';
@@ -11,23 +10,29 @@
 	let event = 'ハックツハッカソン';
 	let team = '閑古鳥';
 	let product = { name: 'kubesmas' };
-	let usedTechnologies = ['kubanetes', 'html'];
+	let usedTechnologies = ['kubernetes', 'html'];
 	let comment = '';
 	function send() {}
+	let coments = ['いいね', 'さいこー'];
 </script>
 
 <Header />
-
-<div class="container mx-auto flex w-3/4 aspect-w-2 aspect-h-1">
-	<Static {name} {github} {twitter} />
-</div>
-
-<div class="container mx-auto relative">
-	<div class="aspect-w-2 aspect-h-1 w-3/4 mx-auto">
-		<Dynamic {event} {team} {product} {usedTechnologies} />
+<div class="container max-w-screen-xl mx-auto px-4">
+	<div class="mx-auto aspect-w-3 aspect-h-1 m-2">
+		<Static {name} {github} {twitter} class="w-3/5 mx-auto border-gray-300 border-2" />
 	</div>
-	<div class="absolute edit-possition bottom-0 ">
-		<div class="rounded edit-bt opacity-75 bg-gray-800">
+
+	<div class="relative mx-auto aspect-w-3 aspect-h-1">
+		<Dynamic
+			{event}
+			{team}
+			{product}
+			{usedTechnologies}
+			class="w-3/5 mx-auto border-gray-300 border-2"
+		/>
+	</div>
+	<div class="relative w-4/5">
+		<div class="absolute bottom-0 right-0 rounded edit-bt m-2 opacity-75 bg-gray-800">
 			<a
 				href="/kaerareru"
 				class="flex justify-center items-center outline-none focus:ring-2 w-full h-full"
@@ -36,27 +41,33 @@
 			</a>
 		</div>
 	</div>
-</div>
 
-<div class="container mx-auto px-8">
-	<p class="t-size">コメント</p>
-	<div class="border-gray-400 border mb-6" />
-	<div class="flex">
-		<Input bind:value={comment} placeholder="" class="w-full" />
-		<div class="flex-grow" />
-		<Button class="bg-blue-600" on:click={send}>
-			<CaretRightGlyph class="h-5 w-5" style="fill:white" />
-		</Button>
+	<div class="mx-auto px-8">
+		<p class="t-size">コメント</p>
+		<div class="border-gray-400 border mb-6" />
+		<div class="flex">
+			<Input bind:value={comment} placeholder="" class="w-full mr-2" />
+			<Button class="bg-blue-600" on:click={send}>
+				<SendFilled32 class="h-5 w-5" style="fill:white" />
+			</Button>
+		</div>
+		<div class="opacity-75">
+			<p>コメントはプロダクト単位、匿名で公開されます</p>
+			<p>誹謗中傷は侮辱罪に該当する場合があります</p>
+		</div>
+		<ul>
+			{#each coments as comment}
+				<li>
+					<p class="t-size m-4">{comment}</p>
+				</li>
+			{/each}
+		</ul>
 	</div>
 </div>
 
 <Footer />
 
 <style>
-	.edit-possition {
-		right: 15vw;
-		bottom: 5vw;
-	}
 	.edit-bt {
 		height: 6vw;
 		width: 6vw;
