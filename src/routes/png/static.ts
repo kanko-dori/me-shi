d01ee0ee93124c0943a0502134e37ea878b9d7e7
ssssota <tommysouta@gmail.com>
2021-09-22 07:40:00 +0900

    add dynamic image generator

---
+++ src/routes/png/static.ts
@@ -0,0 +1,14 @@
+import type { RequestHandler } from '@sveltejs/kit';
+import 'isomorphic-fetch';
+
+export const get: RequestHandler = async ({ query }) => {
+	const url = new URL(`https://svg2png.deno.dev/https://me-shi.ga/svg/static?${query.toString()}`);
+	const arrayBuffer = await fetch(url.toString()).then((res) => res.arrayBuffer());
+
+	return {
+		headers: {
+			'content-type': 'image/png'
+		},
+		body: new Uint8Array(arrayBuffer)
+	};
+};
