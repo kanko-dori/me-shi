a59e962502bd745c05c8d5126a49a1a725add21f
ssssota <tommysouta@gmail.com>
2021-09-21 22:34:25 +0900

    static me-shi onupdate modal

--- src/routes/kaerareru.svelte
+++ src/routes/kaerareru.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { auth, token } from '$lib/auth';
-	import { Button, Footer, Header, Input } from '$lib/components';
+	import { Button, Footer, Header, Input, Modal } from '$lib/components';
 	import { updateUser } from '$lib/graphql/query/updateUser';
 	import { user } from '$lib/store';
 	import { Static } from '$lib/svg';
@@ -9,6 +9,7 @@
 	let name = '';
 	let githubId = '';
 	let twitterId = '';
+	let showOnUpdateModal = false;
 
 	user.subscribe((u) => {
 		if (u.type !== 'success') return;
@@ -38,7 +39,7 @@
 			.then(() => {
 				console.log('updateUser done.');
 				auth.reload();
-				goto('/tsunageru');
+				showOnUpdateModal = true;
 			})
 			.catch((e: Error) => {
 				console.error(e);
@@ -67,12 +68,38 @@
 			<Input class="w-full" bind:value={twitterId} />
 		</div>
 		<div class="p-4 flex">
-			<Button class="hover:shadow transition-shadow bg-gray-300">Cancel</Button>
-			<div class="flex-grow" />
-			<Button class="hover:shadow transition-shadow bg-blue-500 text-white" type="submit"
-				>Apply</Button
+			<Button
+				class="hover:shadow transition-shadow bg-gray-300"
+				on:click={() => goto('/tsunageru')}
 			>
+				Cancel
+			</Button>
+			<div class="flex-grow" />
+			<Button class="hover:shadow transition-shadow bg-blue-500 text-white" type="submit">
+				Apply
+			</Button>
 		</div>
 	</form>
 </main>
+
+<Modal open={showOnUpdateModal}>
+	<div
+		class="max-w-4xl p-8 bg-white rounded-md shadow-md z-20 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+	>
+		<h3 class="py-4">更新しました！</h3>
+		<div class="flex">
+			<div class="flex-grow" />
+			<button
+				class="bg-blue-500 rounded text-white py-2 px-4"
+				on:click={() => {
+					goto('/tsunageru');
+					showOnUpdateModal = false;
+				}}
+			>
+				OK
+			</button>
+		</div>
+	</div>
+</Modal>
+
 <Footer />
