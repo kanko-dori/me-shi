5660fb5eda9d269c3ae47fe8cb3b44eab0687184
kambe <nori.k3636@gmail.com>
2021-09-21 11:54:46 +0900

    fix tailwind's class

--- src/routes/mirareru.svelte
+++ src/routes/mirareru.svelte
@@ -11,6 +11,8 @@
 	let team = '閑古鳥';
 	let product = { name: 'kubesmas' };
 	let usedTechnologies = ['kubernetes', 'html'];
+	let preferendTechnologies = ['graphQL'];
+	let memberOf = 'friend name';
 	let comment = '';
 	const send = () => {
 		console.log('send!');
@@ -18,62 +20,59 @@
 	let coments = ['いいね', 'さいこー'];
 </script>
 
-<Header />
-<div class="container max-w-screen-xl mx-auto px-4">
-	<div class="mx-auto aspect-w-3 aspect-h-1 m-2">
-		<Static {name} {github} {twitter} class="w-3/5 mx-auto border-gray-300 border-2" />
-	</div>
-
-	<div class="relative mx-auto aspect-w-3 aspect-h-1">
-		<Dynamic
-			{event}
-			{team}
-			{product}
-			{usedTechnologies}
-			class="w-3/5 mx-auto border-gray-300 border-2"
-		/>
-	</div>
-	<div class="relative w-4/5">
-		<div class="absolute bottom-0 right-0 rounded edit-bt m-2 opacity-75 bg-gray-800">
+<Header showSignOut={true} />
+<main class="container mx-auto px-4 max-w-3xl">
+	<div class="flex flex-col gap-2 w-full mx-auto p-4">
+		<div>
+			<Static {name} {github} twitter={twitter || undefined} class="w-full shadow-xl" />
+		</div>
+		<div class="relative">
+			<Dynamic
+				{event}
+				{team}
+				{product}
+				{usedTechnologies}
+				preferedTechnologies={preferendTechnologies || undefined}
+				memberOf={memberOf || undefined}
+				class="w-full shadow-xl"
+			/>
 			<a
-				href="/kaerareru"
-				class="flex justify-center items-center outline-none focus:ring-2 w-full h-full"
+				href="/wataseru/${'slug'}"
+				class="absolute flex justify-center items-center outline-none focus:ring-2 w-12 h-12 rounded-md opacity-75 bg-gray-800 right-2 bottom-2"
 			>
 				<QrCode16 class="h-3/5 w-3/5" style="fill:white" />
 			</a>
 		</div>
 	</div>
 
-	<div class="mx-auto px-8">
-		<p class="t-size">コメント</p>
-		<div class="border-gray-400 border mb-6" />
-		<div class="flex">
-			<Input bind:value={comment} placeholder="" class="w-full mr-2" />
-			<Button class="bg-blue-600" on:click={send}>
-				<SendFilled32 class="h-5 w-5" style="fill:white" />
-			</Button>
+	<div class="mx-auto px-8 py-4">
+		<p class="text-2xl p-2 border-b-2 border-gray-300">コメント</p>
+		<div class="py-6">
+			<div class="flex gap-2">
+				<Input bind:value={comment} placeholder="" class="flex-grow" />
+				<Button class="bg-blue-600 px-2 py-2" on:click={send}>
+					<SendFilled32 class="h-6 w-6" style="fill:white " />
+				</Button>
+			</div>
+			<div class="text-xs lg:text-sm text-gray-500">
+				<p>コメントはプロダクト単位、匿名で公開されます</p>
+				<p>誹謗中傷は侮辱罪に該当する場合があります</p>
+			</div>
 		</div>
-		<div class="opacity-75">
-			<p>コメントはプロダクト単位、匿名で公開されます</p>
-			<p>誹謗中傷は侮辱罪に該当する場合があります</p>
-		</div>
-		<ul>
+
+		<ul class="flex flex-col gap-6">
 			{#each coments as comment}
-				<li>
-					<p class="t-size m-4">{comment}</p>
+				<li class="p-2">
+					<span class="text-xl px-4 py-2 rounded shadow border border-gray-200">{comment}</span>
 				</li>
 			{/each}
 		</ul>
 	</div>
-</div>
+</main>
 
 <Footer />
 
 <style>
-	.edit-bt {
-		height: 6vw;
-		width: 6vw;
-	}
 	.t-size {
 		font-size: 3vw;
 	}
