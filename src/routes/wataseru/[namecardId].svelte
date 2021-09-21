3ac2972af29b177d853439acedf6e39caf181779
Takamichi Omori <merrytakamerry@gmail.com>
2021-09-22 06:07:11 +0900

    add onAddNamecard

--- src/routes/wataseru/[namecardId].svelte
+++ src/routes/wataseru/[namecardId].svelte
@@ -3,15 +3,19 @@
 	import { token } from '$lib/auth';
 
 	import { QRcode, Header, Footer } from '$lib/components';
+	import Modal from '$lib/components/Modal.svelte';
 	import { subscription } from '$lib/graphql';
-	import { getNamecard } from '$lib/graphql/query';
+	import { addNamecard, getNamecard } from '$lib/graphql/query';
 	import { Dynamic } from '$lib/svg';
+	import type { AddNamecardResult, AddNamecardResultResolvers } from 'src/generated/graphql';
 
 	let event = '';
 	let team = '';
 	let product = { name: '' };
 	let usedTechnologies: Array<string> = [];
 	let preferTechnologies: Array<string> = [];
+
+	let showOnAddNamecardModal = false;
 
 	token.subscribe((t) => {
 		if (t.type !== 'success') return;
@@ -49,7 +53,20 @@
 					{
 						Authorization: t.value ?? ''
 					}
-				).subscribe(console.log, console.warn, console.log);
+				).subscribe(
+					(res: any) => {
+						const onAddNamecard = res.value.data as AddNamecardResult;
+						console.log('addNamecard was called with: ', onAddNamecard);
+						addNamecard(
+							{ input: onAddNamecard.getterNamecardId ?? '' },
+							{ Authorization: t.value ?? '' }
+						).then(() => {
+							console.log('addNamecard done.');
+						});
+					},
+					console.warn,
+					console.log
+				);
 			});
 	});
 </script>
@@ -83,4 +100,27 @@
 		/>
 	</p>
 </main>
+
+<Modal open={showOnAddNamecardModal}>
+	<div
+		class="max-w-4xl p-8 bg-white rounded-md shadow-md z-20 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+	>
+		<h3 class="py-4">名刺の交換が完了しました！</h3>
+		<h3 class="py-4">交換した名刺を見に行ってみましょう！</h3>
+
+		<div class="flex">
+			<div class="flex-grow" />
+			<button
+				class="bg-blue-500 rounded text-white py-2 px-4"
+				on:click={() => {
+					// goto('/tsunageru');
+					showOnAddNamecardModal = false;
+				}}
+			>
+				OK
+			</button>
+		</div>
+	</div>
+</Modal>
+
 <Footer />
