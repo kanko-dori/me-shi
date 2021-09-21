ac311065947c6dda8ab7d7c17157c21541c98596
Takamichi Omori <merrytakamerry@gmail.com>
2021-09-22 00:08:17 +0900

    use object-hash

--- src/lib/components/QRcode.svelte
+++ src/lib/components/QRcode.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import { Svg } from '$lib/svg';
 	import QRcode from 'qrcode-svg';
+	import hash from 'object-hash';
+
 	export let content: string;
 	export let color: string | { color: string; offset: number }[] = '#000000';
 
-	const id = Math.random().toString(32).substring(2);
+	const id = hash(content);
 	const fill = typeof color === 'string' ? color : `url(#${id})`;
 
 	let qrcode: QRcode = new QRcode(content);
@@ -25,5 +27,5 @@
 			{/if}
 		</linearGradient>
 	</defs>
-	{@html qrcode.svg({ container: 'none' })}
+	{@html qrcode.svg()}
 </Svg>
